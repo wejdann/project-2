0df12c388c044f61c1c9d29313d54b727e85aa14
wejdann <weejdansaud58@hotmail.com>
2020-02-26 16:05:15 +0300

    Updates

---
+++ precache-manifest.c36b9334848dcb1a9b527c1688d0f54b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48a6cf087528954d8ff26b28d354501",
+    "url": "/project-2/index.html"
+  },
+  {
+    "revision": "06605d16c260f2199959",
+    "url": "/project-2/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "82d4f31d70cfd7906134",
+    "url": "/project-2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "06605d16c260f2199959",
+    "url": "/project-2/static/js/2.b235e957.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/project-2/static/js/2.b235e957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82d4f31d70cfd7906134",
+    "url": "/project-2/static/js/main.ed1d74c0.chunk.js"
+  },
+  {
+    "revision": "3a1ae9bcdc3100c2b8e3",
+    "url": "/project-2/static/js/runtime-main.eb49b1de.js"
+  }
+]);
